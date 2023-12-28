92bcba91d0134b27db5c81afb3cf3ab7560f86a9
farihanurhasin20 <fariha.hasin4424@gmail.com>
2023-12-28 15:04:26 +0600

    Register as Merchant

--- database/migrations/2023_12_28_072632_create_organizations_table.php
+++ database/migrations/2023_12_28_072632_create_organizations_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
-            $table->string('role')->default(1);
-            $table->string('name');
-            $table->string('mobile')->unique();
-            $table->string('password');
-            $table->string('address');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('org_name');
+            $table->string('slug');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\merchant\MerchantAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+//for merchant registration and login
+Route::group(['prefix' => 'merchant'], function () {
+    Route::post('/register',[MerchantAuthController::class,'register']);
 });
